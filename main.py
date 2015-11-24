f5ec0d710ee1e984666ec31630419e4859295dbb
purplegoat53 <J.Rose@dundee.ac.uk>
2015-11-24 17:14:58 +0000

    Initial Structure

---
+++ main.py
@@ -0,0 +1,9 @@
+from bottle import route, template, run, static_file, request, response, view
+
+@route("/static/<filename:path>")
+def send_static(filename):
+    response.content_type = mimetypes.guess_type(filename)
+    return static_file(filename, root="./static")
+
+if __name__ == "__main__":
+    run(server="waitress", host='0.0.0.0', reloader=True, port=8080, debug=True)
