fed54ef583de9a94baaa162844f6c5124eb49285
purplegoat53 <J.Rose@dundee.ac.uk>
2015-11-24 17:35:06 +0000

    add index view to backend

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ def send_static(filename):
     response.content_type = mimetypes.guess_type(filename)
     return static_file(filename, root="./static")
 
+@route("/")
+@view("index")
+def index():
+    pass
+
 if __name__ == "__main__":
     run(server="waitress", host='0.0.0.0', reloader=True, port=8080, debug=True)
